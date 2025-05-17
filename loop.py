5d1a25a738e38b15c2c8bb15bcd592b6294eb7ee
Aimee <x9914879@gmail.com>
2025-05-17 12:24:15 +0800

    second commit

--- loop.py
+++ loop.py
@@ -1,10 +1,13 @@
 password = 'a123456'
 x = 3
-while x <= 3 and x > 0:
+while x > 0:
+    x = x - 1
     user = input('Please enter your password :')
     if user != password:
-        print('密碼錯誤! 還有', x - 1, '次機會!')
-        x = x - 1
+        if x > 0:
+            print('密碼錯誤! 還有', x, '次機會!') 
+        if x == 0:
+            print('密碼錯誤! 沒機會了歐~')            
     else:
         print('登入成功!')
         break
